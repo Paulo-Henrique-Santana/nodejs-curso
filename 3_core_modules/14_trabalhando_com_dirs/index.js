445332eb157dfcb2b05c6e8e44e7a4bd71c8e120
Paulo-Henrique-Santana <phenrique.santana@hotmail.com>
2023-03-16 14:17:59 -0300

    Adiciona seções de Core Modules e Fundamentos NPM

---
+++ 3_core_modules/14_trabalhando_com_dirs/index.js
@@ -0,0 +1,8 @@
+const fs = require("fs");
+
+if (!fs.existsSync("./minhapasta")) {
+  console.log("Não existe!");
+  fs.mkdirSync("minhapasta");
+} else {
+  console.log("Existe!");
+}
---
+++ 4_npm_fundamental/6_pacote_global/index.js
@@ -0,0 +1,5 @@
+const _ = require("lodash");
+
+const arr = [1, 2, 3, 3, 3, 4, 4, 5];
+
+console.log(_.sortedUniq(arr));
