ae4bab66a0e8bf795b6a797ac990ac60207c0b9d
Paulo-Henrique-Santana <phenrique.santana@hotmail.com>
2023-04-01 16:54:46 -0300

    Adiciona conteúdo da seção de mvc

---
+++ 10_mvc/7_resgatando_dados/controllers/TaskController.js
@@ -0,0 +1,25 @@
+const Task = require("../models/Task");
+
+module.exports = class TaskController {
+  static createTask(req, res) {
+    res.render("tasks/create");
+  }
+
+  static async createTaskSave(req, res) {
+    const task = {
+      title: req.body.title,
+      description: req.body.description,
+      done: false,
+    };
+
+    await Task.create(task);
+
+    res.redirect("/tasks");
+  }
+
+  static async showTasks(req, res) {
+    const tasks = await Task.findAll({ raw: true });
+
+    res.render("tasks/all", { tasks });
+  }
+};
